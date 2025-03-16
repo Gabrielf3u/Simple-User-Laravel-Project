019722934c252a00ad1aa52cae3b1c9d4b26e452
Gabriel Feuilherade <gabrielf3u@gmail.com.au>
2025-03-17 08:54:42 +1000

    Refactored the deliver of users so they are displayed dynamically

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,18 +7,18 @@
 <body>
     <h2>Currently available users</h2>
     <p>{{ $greeting }}</p>
+
+    @if($greeting == "Hxh characters")
+        <p>If statement</p>
+    @endif
     
     <ul>
-        <li>
-            <a href="/users/{{ $users[0]["id"] }}">
-                {{ $users[0]["name"] }}
-            </a>
-        </li>
-        <li>
-            <a href="/users/{{ $users[1]["id"] }}">
-                {{ $users[1]["name"] }}
-            </a>
-        </li>
+        @foreach($users as $user)
+            <li>
+                <p>{{ $user['name'] }}</p>
+                <a href="/users/{{ $user['id'] }}">View Details</a>
+            </li>
+        @endforeach
     </ul>
 </body>
 </html>

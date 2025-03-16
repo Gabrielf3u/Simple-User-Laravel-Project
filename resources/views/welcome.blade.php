5dff2c0f2da98e1f13ce221c43ecd87f431c03d4
Gabriel Feuilherade <gabrielf3u@gmail.com.au>
2025-03-17 09:52:24 +1000

    Incooperated CSS using Tailwind for improved asthetics and UX

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Network</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body class="text-center px-8 py-12">
     <h1>Welcome to the User Network </h1>
     <p>Select the following button to view the list of users.</p>
 
-    <a href="/users" class="btn">
+    <a href="/users" class="btn mt-4 inline-block">
         Locate Users
     </a>
 </body>

f2dd688ad790975e5e4c810ba231cfcc070042c7
Gabriel Feuilherade <gabrielf3u@gmail.com.au>
2025-03-17 13:36:41 +1000

    Added error rendering and data rentention for failed character creation from

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -4,7 +4,7 @@
 
         <h2>Create a New Character</h2>
 
-        <!-- ninja Name -->
+        <!-- Character Name -->
         <label for="name">Character Name:</label>
         <input 
             type="text" 
@@ -20,6 +20,7 @@
             type="number" 
             id="age" 
             name="age" 
+            value="{{ old('age') }}"
             required
         >
 
@@ -30,15 +31,15 @@
             id="bio" 
             name="bio" 
             required
-        ></textarea>
+        >{{ old('bio') }}</textarea>
 
-        <!-- select a dojo -->
+        <!-- select a Origin -->
         <label for="origin_id">Origins:</label>
         <select id="origin_id" name="origin_id" required>
             <option value="" disabled selected>Select a origin</option>
 
             @foreach($origins as $origin)
-                <option value="{{ $origin->id }}">
+                <option value="{{ $origin->id }}" {{ $origin->id == old('origin_id') ? 'selected' : '' }}>
                     {{ $origin->name }}
                 </option>
             @endforeach
@@ -48,6 +49,12 @@
         <button type="submit" class="btn mt-4">Create Character</button>
 
         <!-- validation errors -->
+        @if ($errors->any())
+            <ul class="ul.px-4.py-2.bg-red-100">
+                @foreach ($errors->all() as $error)
+                    <li class="my-2 text-red-500"> {{ $error }}</li>
+                @endforeach
+        @endif
         
     </form>
 </x-layout>
